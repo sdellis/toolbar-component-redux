4a266868e0131620e75d9b8abdef0c32e4947a62
Shaun Ellis <shaun@sdellis.com>
2016-11-09 15:15:31 -0500

    testing jsviews

--- dist/toolbar-component.d.ts
+++ dist/toolbar-component.d.ts
@@ -25,7 +25,7 @@ declare namespace IIIFComponents {
     class ToolbarComponent extends _Components.BaseComponent {
         options: IToolbarComponentOptions;
         private _$toolbar;
-        private store;
+        private _buttons;
         constructor(options: IToolbarComponentOptions);
         test(): void;
         protected _init(): boolean;
